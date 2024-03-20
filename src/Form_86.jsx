49b76b4c66133bdf5c90ab9c390020f98fa0ab12
ivan930509 <ivan9305090919@gmail.com>
2024-03-20 21:57:49 +0800

    add task

--- src/Form_86.jsx
+++ src/Form_86.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react';
+import { useMutation ,useQueryClient} from '@tanstack/react-query';
+import { supabase } from './db/clientSupabase';
+import { toast } from 'react-toastify';
 
 const Form_86 = () => {
   const [newItemName, setNewItemName] = useState('');
+  const queryClient = useQueryClient();
+  const { mutate:createTask , isLoading} = useMutation({
+    mutationFn: async(title)=>{
+      try{
+        const { data, error } = await supabase
+        .from('task_86')
+        .insert([{ title, is_done: false }])
+        .select();  
+      }catch(error){
+        console.log(error);
+      }
+    },
+    onSuccess: ()=>{
+      queryClient.invalidateQueries({
+        queryKey:['tasks']
+      });
+      toast.success('task added');
+      setNewItemName('');
+    },
+    onError : (error) =>{
+      toast.error(error.response.data);
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(newItemName === ''){
+      toast.warning('Please provide title');
+    }else{
+      createTask(newItemName);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
-      <h4>task bud -- htchung</h4>
+      <h4>task bud -- 周承平</h4>
       <div className='form-control'>
         <input
           type='text '

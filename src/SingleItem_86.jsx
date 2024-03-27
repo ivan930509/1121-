ec91afd8cfcde7968830ae47f76f25a4952cfc5d
ivan930509 <ivan9305090919@gmail.com>
2024-03-27 19:56:16 +0800

    update delete

--- src/SingleItem_86.jsx
+++ src/SingleItem_86.jsx
@@ -1,15 +1,60 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { supabase } from './db/clientSupabase';
+import { toast } from 'react-toastify';
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: async ({ taskId, isDone }) => {
+      try {
+        await supabase
+          .from('task_86')
+          .update({ is_done: isDone })
+          .eq('id', taskId);
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('Task updated');
+    }
+  });
+  
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: async (taskId) => {
+      try {
+        await supabase
+          .from('task_86')
+          .delete()
+          .eq('id', taskId);
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('Task deleted');
+    }
+  });
+
   return (
     <div className='single-item'>
       <input
         type='checkbox'
-        checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        checked={item.is_done}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.is_done })}
       />
       <p
         style={{
           textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textDecoration: item.is_done && 'line-through',
         }}
       >
         {item.title}
@@ -17,11 +62,12 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
     </div>
   );
 };
+
 export default SingleItem;
